791ba1cff5fd9dee9ad8af9b94fa113cdf8c2078
crazycs520 <crazycs520@gmail.com>
2021-03-30 20:08:18 +0800

    refine code and remove log
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- src/runtime/stats.go
+++ src/runtime/stats.go
@@ -7,22 +7,17 @@ package runtime
 
 var stats struct {
 	enabled bool // set when collect runtime statistics
-	debug   bool
 }
 
+// EnableStats enable runtime to collect goroutine stats, such as:
+// exec time, schedule wait time, network wait time, syscall time, GC sweep time.
 func EnableStats() {
-	//stopTheWorldGC("start collecting stats")
 	stats.enabled = true
-	//startTheWorldGC()
 }
 
-func EnableStatsDebug() {
-	stats.debug = true
-}
-
+// EnableStats disable runtime to collect goroutine stats.
 func DisableStats() {
 	stats.enabled = false
-	stats.debug = false
 }
 
 type GStats struct {
@@ -82,9 +77,6 @@ func (s *GStats) TotalTime() int64 {
 func (s *GStats) recordGoCreate() {
 	s.blockSchedTime = nanotime()
 	s.creationTime = s.blockSchedTime
-	if stats.debug {
-		print("GoCreate", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGoStart() {
@@ -92,9 +84,6 @@ func (s *GStats) recordGoStart() {
 	if s.blockSchedTime != 0 {
 		s.schedWaitTime += s.lastStartTime - s.blockSchedTime
 		s.blockSchedTime = 0
-	}
-	if stats.debug {
-		print("GoStart", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
 	}
 }
 
@@ -106,9 +95,6 @@ func (s *GStats) recordGoSched() {
 
 	}
 	s.blockSchedTime = ts
-	if stats.debug {
-		print("GoSched", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGoBlock() {
@@ -116,9 +102,6 @@ func (s *GStats) recordGoBlock() {
 	if s.lastStartTime != 0 {
 		s.execTime += ts - s.lastStartTime
 		s.lastStartTime = 0
-	}
-	if stats.debug {
-		print("GoBlock", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
 	}
 }
 
@@ -153,9 +136,6 @@ func (s *GStats) recordGoPark(traceEv byte) {
 			s.lastStartTime = 0
 		}
 	}
-	if stats.debug {
-		print(EventDescriptions[traceEv].Name, ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGoUnpark() {
@@ -169,9 +149,6 @@ func (s *GStats) recordGoUnpark() {
 		s.blockSyncTime = 0
 	}
 	s.blockSchedTime = ts
-	if stats.debug {
-		print("GoUnpark", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGoSysBlock() {
@@ -181,9 +158,6 @@ func (s *GStats) recordGoSysBlock() {
 		s.lastStartTime = 0
 	}
 	s.blockSyscallTime = ts
-	if stats.debug {
-		print("GoSysBlock", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGoSysExit() {
@@ -193,9 +167,6 @@ func (s *GStats) recordGoSysExit() {
 		s.blockSyscallTime = 0
 	}
 	s.blockSchedTime = ts
-	if stats.debug {
-		print("GoSysExit", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGCSweepStart() {
@@ -203,9 +174,6 @@ func (s *GStats) recordGCSweepStart() {
 		return
 	}
 	s.blockSweepTime = nanotime()
-	if stats.debug {
-		print("GoGCSweepStart", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) recordGCSweepDone() {
@@ -217,9 +185,6 @@ func (s *GStats) recordGCSweepDone() {
 		s.sweepTime += nanotime() - s.blockSweepTime
 		s.blockSweepTime = 0
 	}
-	if stats.debug {
-		print("GoGCSweepDone", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) isEnd() bool {
@@ -230,16 +195,11 @@ func (s *GStats) recordGoEnd() {
 	ts := nanotime()
 	s.finalize(ts)
 	s.endTime = ts
-	if stats.debug {
-		print("GoEnd", ", go: ", s.goid, " exec: ", s.execTime/1000000, "\n")
-	}
 }
 
 func (s *GStats) finalize(ts int64) {
 	if s.creationTime != 0 {
 		s.totalTime = ts - s.creationTime
-	} else {
-		s.totalTime = s.execTime + s.ioTime + s.blockTime + s.syscallTime + s.schedWaitTime + s.blockSweepTime
 	}
 	if s.lastStartTime != 0 {
 		s.execTime += ts - s.lastStartTime
@@ -262,73 +222,21 @@ func (s *GStats) finalize(ts int64) {
 }
 
 // public some runtime api.
-
 // GetGID retrieves the goroutine (G's) ID.
 func GetGID() int64 {
-	return getg().m.curg.goid
+	_g_ := getg().m.curg
+	if _g_ != nil {
+		return _g_.goid
+	}
+	return -1
 }
 
 // GetGID retrieves the goroutine (G's) runtime stats.
 func GetGStats() *GStats {
 	_g_ := getg().m.curg
-	_g_.stats.recordGoEnd()
-	return &_g_.stats
-}
-
-var EventDescriptions = [traceEvCount]struct {
-	Name       string
-	minVersion int
-	Stack      bool
-	Args       []string
-	SArgs      []string // string arguments
-}{
-	traceEvNone:              {"None", 1005, false, []string{}, nil},
-	traceEvBatch:             {"Batch", 1005, false, []string{"p", "ticks"}, nil}, // in 1.5 format it was {"p", "seq", "ticks"}
-	traceEvFrequency:         {"Frequency", 1005, false, []string{"freq"}, nil},   // in 1.5 format it was {"freq", "unused"}
-	traceEvStack:             {"Stack", 1005, false, []string{"id", "siz"}, nil},
-	traceEvGomaxprocs:        {"Gomaxprocs", 1005, true, []string{"procs"}, nil},
-	traceEvProcStart:         {"ProcStart", 1005, false, []string{"thread"}, nil},
-	traceEvProcStop:          {"ProcStop", 1005, false, []string{}, nil},
-	traceEvGCStart:           {"GCStart", 1005, true, []string{"seq"}, nil}, // in 1.5 format it was {}
-	traceEvGCDone:            {"GCDone", 1005, false, []string{}, nil},
-	traceEvGCSTWStart:        {"GCSTWStart", 1005, false, []string{"kindid"}, []string{"kind"}}, // <= 1.9, args was {} (implicitly {0})
-	traceEvGCSTWDone:         {"GCSTWDone", 1005, false, []string{}, nil},
-	traceEvGCSweepStart:      {"GCSweepStart", 1005, true, []string{}, nil},
-	traceEvGCSweepDone:       {"GCSweepDone", 1005, false, []string{"swept", "reclaimed"}, nil}, // before 1.9, format was {}
-	traceEvGoCreate:          {"GoCreate", 1005, true, []string{"g", "stack"}, nil},
-	traceEvGoStart:           {"GoStart", 1005, false, []string{"g", "seq"}, nil}, // in 1.5 format it was {"g"}
-	traceEvGoEnd:             {"GoEnd", 1005, false, []string{}, nil},
-	traceEvGoStop:            {"GoStop", 1005, true, []string{}, nil},
-	traceEvGoSched:           {"GoSched", 1005, true, []string{}, nil},
-	traceEvGoPreempt:         {"GoPreempt", 1005, true, []string{}, nil},
-	traceEvGoSleep:           {"GoSleep", 1005, true, []string{}, nil},
-	traceEvGoBlock:           {"GoBlock", 1005, true, []string{}, nil},
-	traceEvGoUnblock:         {"GoUnblock", 1005, true, []string{"g", "seq"}, nil}, // in 1.5 format it was {"g"}
-	traceEvGoBlockSend:       {"GoBlockSend", 1005, true, []string{}, nil},
-	traceEvGoBlockRecv:       {"GoBlockRecv", 1005, true, []string{}, nil},
-	traceEvGoBlockSelect:     {"GoBlockSelect", 1005, true, []string{}, nil},
-	traceEvGoBlockSync:       {"GoBlockSync", 1005, true, []string{}, nil},
-	traceEvGoBlockCond:       {"GoBlockCond", 1005, true, []string{}, nil},
-	traceEvGoBlockNet:        {"GoBlockNet", 1005, true, []string{}, nil},
-	traceEvGoSysCall:         {"GoSysCall", 1005, true, []string{}, nil},
-	traceEvGoSysExit:         {"GoSysExit", 1005, false, []string{"g", "seq", "ts"}, nil},
-	traceEvGoSysBlock:        {"GoSysBlock", 1005, false, []string{}, nil},
-	traceEvGoWaiting:         {"GoWaiting", 1005, false, []string{"g"}, nil},
-	traceEvGoInSyscall:       {"GoInSyscall", 1005, false, []string{"g"}, nil},
-	traceEvHeapAlloc:         {"HeapAlloc", 1005, false, []string{"mem"}, nil},
-	traceEvNextGC:            {"NextGC", 1005, false, []string{"mem"}, nil},
-	traceEvTimerGoroutine:    {"TimerGoroutine", 1005, false, []string{"g"}, nil}, // in 1.5 format it was {"g", "unused"}
-	traceEvFutileWakeup:      {"FutileWakeup", 1005, false, []string{}, nil},
-	traceEvString:            {"String", 1007, false, []string{}, nil},
-	traceEvGoStartLocal:      {"GoStartLocal", 1007, false, []string{"g"}, nil},
-	traceEvGoUnblockLocal:    {"GoUnblockLocal", 1007, true, []string{"g"}, nil},
-	traceEvGoSysExitLocal:    {"GoSysExitLocal", 1007, false, []string{"g", "ts"}, nil},
-	traceEvGoStartLabel:      {"GoStartLabel", 1008, false, []string{"g", "seq", "labelid"}, []string{"label"}},
-	traceEvGoBlockGC:         {"GoBlockGC", 1008, true, []string{}, nil},
-	traceEvGCMarkAssistStart: {"GCMarkAssistStart", 1009, true, []string{}, nil},
-	traceEvGCMarkAssistDone:  {"GCMarkAssistDone", 1009, false, []string{}, nil},
-	traceEvUserTaskCreate:    {"UserTaskCreate", 1011, true, []string{"taskid", "pid", "typeid"}, []string{"name"}},
-	traceEvUserTaskEnd:       {"UserTaskEnd", 1011, true, []string{"taskid"}, nil},
-	traceEvUserRegion:        {"UserRegion", 1011, true, []string{"taskid", "mode", "typeid"}, []string{"name"}},
-	traceEvUserLog:           {"UserLog", 1011, true, []string{"id", "keyid"}, []string{"category", "message"}},
+	if _g_ != nil {
+		_g_.stats.recordGoEnd()
+		return &_g_.stats
+	}
+	return nil
 }
